16e71c221ae29e9c3b8488d9ba99f40352865543
Luis Antonio Vázquez García <luis.vazquezga@udlap.mx>
2026-02-02 21:24:03 -0600

    feat: session de usuario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,12 @@ import { HomeComponent } from './pages/home/home.component';
 import { CreateSessionComponent } from './pages/create-session/create-session.component';
 import { JoinSessionComponent } from './pages/join-session/join-session.component';
 import { SessionComponent } from './pages/session/session.component';
+import { AthleteComponent } from './pages/athlete/athlete.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'create', component: CreateSessionComponent },
-  { path: 'join', component: JoinSessionComponent },
+  { path: 'join', component: AthleteComponent },
   { path: 'session/:id', component: SessionComponent },
   { path: '**', redirectTo: '' }
 ];
---
+++ src/app/pages/athlete/athlete.component.ts
@@ -0,0 +1,77 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-athlete',
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule, RouterModule],
+  templateUrl: './athlete.component.html',
+  styleUrl: './athlete.component.css'
+})
+export class AthleteComponent implements OnInit {
+
+  form!: FormGroup;
+
+  // 🔴 Simula el template que vendría del coach
+  sessionTemplate = {
+    exercises: [
+      {
+        name: 'Sentadilla',
+        columns: [
+          { label: 'Peso', type: 'number' },
+          { label: 'Reps', type: 'number' }
+        ]
+      },
+      {
+        name: 'Plancha',
+        columns: [
+          { label: 'Tiempo', type: 'time' }
+        ]
+      }
+    ]
+  };
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      athleteName: [''],
+      exercises: this.fb.array([])
+    });
+
+    this.buildFromTemplate();
+  }
+
+  get exercises(): FormArray {
+    return this.form.get('exercises') as FormArray;
+  }
+
+  buildFromTemplate() {
+    this.sessionTemplate.exercises.forEach(ex => {
+      this.exercises.push(
+        this.fb.group({
+          name: [ex.name],
+          values: this.fb.array(
+            ex.columns.map(col =>
+              this.fb.group({
+                label: [col.label],
+                type: [col.type],
+                value: ['']
+              })
+            )
+          )
+        })
+      );
+    });
+  }
+
+  getValues(i: number): FormArray {
+    return this.exercises.at(i).get('values') as FormArray;
+  }
+
+  finishSession() {
+    console.log(this.form.value);
+  }
+}
