669fbd91068f99cb33e9477ff513ba167c2a9c5f
Luis Antonio Vázquez García <luis.vazquezga@udlap.mx>
2026-02-02 19:51:44 -0600

    setup routing and base pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { CreateSessionComponent } from './pages/create-session/create-session.component';
+import { JoinSessionComponent } from './pages/join-session/join-session.component';
+import { SessionComponent } from './pages/session/session.component';
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'create', component: CreateSessionComponent },
+  { path: 'join', component: JoinSessionComponent },
+  { path: 'session/:id', component: SessionComponent },
+  { path: '**', redirectTo: '' }
+];
